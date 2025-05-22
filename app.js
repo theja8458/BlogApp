d9cdbcf52f3dca27b00722541608788500d45d57
Theja <sugamanch@gmail.com>
2025-05-22 15:52:23 +0530

    Added admin panel

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ const expressLayout = require("express-ejs-layouts")
 const connectDB = require("./server/config/db.js")
 const app = express();
 const port = 8080 || process.env.port;
-
+const cookieParser = require("cookie-parser");
+const MongoStore = require("connect-mongo");
+const session = require("express-session");
 
 //conncet database
 connectDB();
@@ -12,6 +14,17 @@ connectDB();
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 app.use(express.static("public"));
+app.use(cookieParser());
+
+app.use(session({
+    secret: "Keyboard cat",
+    resave: false,
+    saveUninitialized: true,
+    store : MongoStore.create({
+        mongoUrl:"mongodb://127.0.0.1:27017/blog"
+    }),
+    
+}))
 //template engine
 app.use(expressLayout);
 app.set('layout','./layouts/main');
@@ -19,6 +32,7 @@ app.set("view engine","ejs");
 
 
 app.use('/',require("./server/routes/main"));
+app.use('/',require("./server/routes/admin"));
 app.listen(port,()=>{
     console.log(`App listening on port ${port}`);
 });
---
+++ server/routes/admin.js
@@ -0,0 +1,104 @@
+const express = require("express");
+const router = express.Router();
+const Post = require("../models/Post.js");
+const User = require("../models/User.js");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+const adminLayout  = "../views/layouts/admin";
+
+
+const authMiddleware = (req,res,next)=>{
+  const token = req.cookies.token;
+  if(!token){
+    return res.status(401).json({message:"Unauthorie"});
+  }
+
+  try{
+   const decoded = jwt.verify(token, "Keyboard cat");
+   req.userId = decoded.userId;
+   next();
+  }catch(err){
+    return res.status(401).json({message:"Unauthorie"});
+  }
+};
+
+router.get("/admin",async (req,res)=>{
+    try{
+    const locals = {
+        title:"Admin",
+        description: "Simple blog created with NodeJs, Express and MongoDB"
+    }
+    const data = Post.find();
+   res.render('admin/index',{locals,layout: adminLayout});
+   }catch(err){
+    console.log(err);
+   }
+});
+
+
+router.post("/admin",async (req,res)=>{
+    try{
+    
+   const {username, password} = req.body;
+    const user = await User.findOne({username:username});
+     if(!user){
+        res.status(401).json({message: "Invalid credentials"});
+     }
+     const isPassValid = await bcrypt.compare(password,user.password);
+
+     if(!isPassValid){
+        res.status(401).json({message: "Invalid credentials"});
+     };
+
+     const token = jwt.sign({userId: user._id},"Keyboard cat");
+     res.cookie("token",token,{httpOnly:true});
+
+     res.redirect("/dashboard");
+   }catch(err){
+    console.log(err);
+   }
+});
+
+
+router.get("/dashboard",authMiddleware,async (req,res)=>{
+    res.render("admin/dashboard");
+})
+
+
+
+// router.post("/admin",async (req,res)=>{
+//     try{
+    
+//    const {username, password} = req.body;
+    
+//    }catch(err){
+//     console.log(err);
+//    }
+// });
+
+
+router.post("/register",async (req,res)=>{
+    try{
+    
+   const {username, password} = req.body;
+    
+   const  hashedPassword = await bcrypt.hash(password,10);
+   try{
+   const user = await User.create({username,password: hashedPassword});
+   res.status(201).json({message: "User created",user});
+   }catch(err){
+   if(err.code === 11000){
+    res.status(409).json({message:"User already in use"});
+   }
+   res.status(5000).json({message:"Internal server error"});
+   }
+   }catch(err){
+    console.log(err);
+   }
+});
+
+
+
+
+module.exports = router;
