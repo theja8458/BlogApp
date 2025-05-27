567285023f7806ef0a6185089c48a484413d5992
Theja <sugamanch@gmail.com>
2025-05-27 16:18:38 +0530

    .

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const methodOverride = require("method-override");
 const connectDB = require("./server/config/db.js");
 const {isActiveRoute} = require("./server/helpers/routeHelpers.js");
 const app = express();
-const port = 8080 || process.env.port;
+const port = process.env.PORT || 8080;
 const cookieParser = require("cookie-parser");
 const MongoStore = require("connect-mongo");
 const session = require("express-session");
@@ -24,7 +24,7 @@ app.use(session({
     resave: false,
     saveUninitialized: true,
     store : MongoStore.create({
-        mongoUrl:"mongodb://127.0.0.1:27017/blog"
+        mongoUrl:process.env.MONGODB_URI
     }),
     
 }))
